6f78e53fcf70073f7af9d507b901afc69c47030d
Lenno Sousa <lenno_sousa@hotmail.com>
2018-06-13 11:42:10 -0300

    panel to button

--- AdministrationClinicalSystem/AdministrationClinicalSystem/br.com.acs.view/ACSMainPanel.Designer.cs
+++ AdministrationClinicalSystem/AdministrationClinicalSystem/br.com.acs.view/ACSMainPanel.Designer.cs
@@ -51,10 +51,10 @@
             this.subMenu = new System.Windows.Forms.Panel();
             this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.metroStyleManagerMain)).BeginInit();
             this.panel1.SuspendLayout();
             this.menu.SuspendLayout();
-            this.BtnMeusDados.SuspendLayout();
             this.contentPanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -176,10 +176,8 @@
             // BtnMeusDados
             // 
             this.BtnMeusDados.BackColor = System.Drawing.Color.Transparent;
-            this.BtnMeusDados.Controls.Add(this.metroLabel3);
-            this.BtnMeusDados.Controls.Add(this.nomeUsuario);
             this.BtnMeusDados.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.BtnMeusDados.Location = new System.Drawing.Point(60, 20);
+            this.BtnMeusDados.Location = new System.Drawing.Point(244, 40);
             this.BtnMeusDados.Name = "BtnMeusDados";
             this.BtnMeusDados.Size = new System.Drawing.Size(200, 40);
             this.BtnMeusDados.TabIndex = 0;
@@ -223,7 +221,7 @@
             this.nomeUsuario.BackColor = System.Drawing.Color.Transparent;
             this.nomeUsuario.FontWeight = MetroFramework.MetroLabelWeight.Regular;
             this.nomeUsuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.nomeUsuario.Location = new System.Drawing.Point(21, 10);
+            this.nomeUsuario.Location = new System.Drawing.Point(83, 31);
             this.nomeUsuario.Name = "nomeUsuario";
             this.nomeUsuario.Size = new System.Drawing.Size(46, 19);
             this.nomeUsuario.TabIndex = 19;
@@ -238,7 +236,7 @@
             this.Ola.BackColor = System.Drawing.Color.Transparent;
             this.Ola.FontWeight = MetroFramework.MetroLabelWeight.Regular;
             this.Ola.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.Ola.Location = new System.Drawing.Point(272, 40);
+            this.Ola.Location = new System.Drawing.Point(483, 92);
             this.Ola.Name = "Ola";
             this.Ola.Size = new System.Drawing.Size(33, 19);
             this.Ola.TabIndex = 17;
@@ -326,11 +324,12 @@
             // 
             // contentPanel
             // 
+            this.contentPanel.Controls.Add(this.Ola);
             this.contentPanel.Controls.Add(this.metroLabel2);
+            this.contentPanel.Controls.Add(this.BtnMeusDados);
             this.contentPanel.Controls.Add(this.button1);
             this.contentPanel.Controls.Add(this.subMenu);
             this.contentPanel.Controls.Add(this.metroLabel1);
-            this.contentPanel.Controls.Add(this.Ola);
             this.contentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.contentPanel.Location = new System.Drawing.Point(60, 60);
             this.contentPanel.Name = "contentPanel";
@@ -352,7 +351,7 @@
             this.metroLabel3.BackColor = System.Drawing.Color.Transparent;
             this.metroLabel3.FontWeight = MetroFramework.MetroLabelWeight.Regular;
             this.metroLabel3.ForeColor = System.Drawing.Color.LimeGreen;
-            this.metroLabel3.Location = new System.Drawing.Point(3, 11);
+            this.metroLabel3.Location = new System.Drawing.Point(66, 31);
             this.metroLabel3.Name = "metroLabel3";
             this.metroLabel3.Size = new System.Drawing.Size(21, 19);
             this.metroLabel3.TabIndex = 21;
@@ -373,14 +372,30 @@
             this.button1.TabIndex = 20;
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(60, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 40);
+            this.button2.TabIndex = 21;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.BtnMeusDados_Click);
+            // 
             // ACSMainPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1018, 600);
-            this.Controls.Add(this.BtnMeusDados);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.nomeUsuario);
             this.Controls.Add(this.menu);
             this.Controls.Add(this.contentPanel);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnMenu);
             this.Name = "ACSMainPanel";
@@ -390,11 +405,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.metroStyleManagerMain)).EndInit();
             this.panel1.ResumeLayout(false);
             this.menu.ResumeLayout(false);
-            this.BtnMeusDados.ResumeLayout(false);
-            this.BtnMeusDados.PerformLayout();
             this.contentPanel.ResumeLayout(false);
             this.contentPanel.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -420,6 +434,7 @@
         private System.Windows.Forms.Panel subMenu;
         private MetroFramework.Controls.MetroLabel metroLabel3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
